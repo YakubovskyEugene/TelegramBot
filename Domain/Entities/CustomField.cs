b137cf095e4de52e00cdf1da77dfc5e2ae8a020d
YakubovskyEugene <yakubovskyeugene@gmail.com>
2024-04-09 01:55:35 +0300

    Добавьте файлы проекта.

---
+++ Domain/Entities/CustomField.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class CustomField : BaseEntity
+    {
+        /// <summary>
+        /// Название поля
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// Значение поля
+        /// </summary>
+        public string Value { get; set; }
+    }
+}
---
+++ Domain/Primitives/Gender.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Primitives
+{
+    public enum Gender
+    {
+        Undefined = 0,
+        Man = 1,
+        Woman = 2,
+        Other = 3
+    }
+}
